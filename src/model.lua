a0b57914568cb5a9697ed9f2a912e838f9fc7702
Alfredo Canziani <alfredo.canziani@gmail.com>
2016-02-23 17:56:03 -0500

    Fix bad assumption for working directory
    If th is run one layer above, then the working directory is the layer
    above by default, even for scripts that live layers below.

--- src/model.lua
+++ src/model.lua
@@ -2,14 +2,14 @@ assert(opt, "Options need to be parsed before including the profiler!")
 
 if opt.definition ~= '' then
     -- TODO: what is the convention for definition files? Assuming createModel()
-    paths.dofile(path.join('..', opt.definition))
+    paths.dofile(opt.definition)
     model = createModel(opt.nGPU)
 elseif opt.checkpoint ~= '' then
     require 'nn'
     require 'cunn'
     require 'cudnn'
     require 'nngraph'
-    model = torch.load(path.join('..', opt.checkpoint))
+    model = torch.load(opt.checkpoint)
 else
     assert(false, "Don't know how to load the model")
 end
